773b045f8a1c2902a65d173c31804bf34178f8a9
Gabriel Kuroda <gabrielkuroda20@gmail.com>
2022-06-14 19:32:34 -0300

    NPC and Dialog, GameManager, EssencialsLoader, Fade

---
+++ TCC-CC-GAME/Assets/Scripts/Dialog/DialogManager.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogManager : IPersistentSingleton<DialogManager>
+{
+    public Text dialogText;
+    public Text nameText;
+    public GameObject dialogBox;
+    public GameObject nameBox;
+
+    public string[] dialogLines;
+
+    public int currentLine;
+    private bool justStarted;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (dialogBox.activeInHierarchy)
+        {
+            if (Input.GetButtonUp("Fire1"))
+            {
+                if (!justStarted)
+                {
+                    currentLine++;
+                    if (currentLine >= dialogLines.Length)
+                    {
+                        dialogBox.SetActive(false);
+
+                        GameManager.Instance.dialogActive = false;
+                    }
+                    else
+                    {
+                        checkIfName();
+
+                        dialogText.text = dialogLines[currentLine];
+                    }
+                }
+                else
+                {
+                    justStarted = false;
+                }
+            }
+        }
+    }
+
+    public void ShowDialog(string[] newLines, bool isPerson)
+    {
+        dialogLines = newLines;
+        currentLine = 0;
+
+        checkIfName();
+
+        dialogText.text = dialogLines[currentLine];
+
+        dialogBox.SetActive(true);
+        justStarted = true;
+
+        nameBox.SetActive(isPerson);
+        GameManager.Instance.dialogActive = true;
+    }
+
+    public void checkIfName()
+    {
+        if (dialogLines[currentLine].StartsWith("n-"))
+        {
+            nameText.text = dialogLines[currentLine].Replace("n-", "");
+            currentLine++;
+        }
+    }
+}
